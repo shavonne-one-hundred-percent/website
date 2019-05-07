6502983f579a3bbc4bbca50bf1c12edc20cbb26b
Matthew Janicak <janicak@gmail.com>
2019-05-06 23:18:42 -0700

    modify index and page templates to query and display transformed markdown

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+class IndexPage extends React.Component {
+  render(){
+    console.log(this.props)
+    const post = this.props.data.allMarkdownRemark.nodes[0]
+    const siteTitle = this.props.data.site.siteMetadata.title
+    const siteDescription = this.props.data.site.siteMetadata.description
+  
+    return (
+      <Layout location="home" title={siteTitle}>
+          <SEO 
+            title="Home"
+            description={siteDescription} 
+          />
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />    
+      </Layout>
+    )
+    
+  }
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    allMarkdownRemark(
+      filter: { fields: { slug: { eq: "/" } }}
+    ) {
+    	nodes {
+        html
+    	}
+  	}
+  }
+`
