2e488e9b9901551beb20ce830d6f011cd8d7eba6
Matthew Janicak <janicak@gmail.com>
2019-05-06 23:17:03 -0700

    establish markdown content ingest

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,56 @@ module.exports = {
     title: `Shavonne One Hundred Percent`,
     description: `Shavonne O’Loughlin's Story of Recovery.`,
     author: `@janicak`,
+    homePage: 'home',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: 'assets',
+        path: `${__dirname}/content/assets`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'homePage',
+        path: `${__dirname}/content/home_page`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: 'pages',
+        path: `${__dirname}/content/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              // `ignoreFileExtensions` defaults to [`png`, `jpg`, `jpeg`, `bmp`, `tiff`]
+              // as we assume you'll use gatsby-remark-images to handle
+              // images in markdown as it automatically creates responsive
+              // versions of images.
+              //
+              // If you'd like to not use gatsby-remark-images and just copy your
+              // original images to the public directory, set
+              // `ignoreFileExtensions` to an empty array.
+              ignoreFileExtensions: [],
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+        ],
       },
     },
     `gatsby-transformer-sharp`,
@@ -24,7 +66,7 @@ module.exports = {
         background_color: `#dddddd`,
         theme_color: `#dddddd`,
         display: `minimal-ui`,
-        icon: `src/images/white-hat-icon.png`, // This path is relative to the root of the site.
+        icon: `content/assets/white-hat-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,64 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require(`path`)
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const page = path.resolve(`./src/templates/page.js`)
+  return graphql(
+    `
+      {
+        pages: allMarkdownRemark {
+          edges {
+            node {
+              fields {
+                slug
+              }
+              frontmatter {
+                title
+              }
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+    if (result.errors) {
+      throw result.errors
+    }
+
+    const posts = result.data.pages.edges
+
+    posts.forEach((post) => {
+      const slug = post.node.fields.slug
+        if (slug !== '/') {
+            createPage({
+                path: post.node.fields.slug,
+                component: page,
+                context: {
+                    slug: post.node.fields.slug,
+                },
+            })
+        }
+    })
+
+    return null
+
+  })
+}
+
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const value = createFilePath({ node, getNode })
+    createNodeField({
+      name: `slug`,
+      node,
+      value,
+    })
+  }
+}
